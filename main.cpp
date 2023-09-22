17bb1987483a82cdb4b01e4e66bcdad4fbe119aa
mahdizynali <zeinali.mahdy@.com>
2023-09-22 12:58:00 +0330

    add base main file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+
+class mazeNet{
+
+    private :
+        int input_size, output_size, hidden_size;
+
+    public :
+
+};
+
+int main(){
+
+    mazeNet maze(784, 128, 10);
+
+    return 0;
+}
