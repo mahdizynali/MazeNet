479f64a560cc9d98844d19821b72cae0278ad95a
mahdizynali <zeinali.mahdy@.com>
2023-09-22 13:19:52 +0330

    basic public functions

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ class mazeNet{
 
     private :
         int input_size, output_size, hidden_size;
+        cv::Mat w1, b1, w2, b2;
+        cv::Mat z1, a1, z2, result;
 
     public :
-
+        mazeNet(int, int, int);
+        cv::Mat forward(const cv::Mat &);
+        void backward();
+        void printLayerSize();
 };
 
 int main(){
