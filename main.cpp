ec705558400e460f78fdfbe2ea2d9aa1ed6e5ff1
mahdizynali <zeinali.mahdy@.com>
2023-09-22 14:35:36 +0330

    complete the relu fucntion

--- main.cpp
+++ main.cpp
@@ -10,21 +10,38 @@ class utils{
         cv::Mat dot (const cv::Mat &, const cv::Mat &);
 };
 
-cv::Mat utils :: dot (const cv::Mat &mat1, const cv::Mat &mat2) {
+cv::Mat utils :: relu (const cv::Mat & X) {
+    // cv::Mat tmp = X;
+    // for (int i=0; i<tmp.rows; i++){
+    //     for(int j=0; j<tmp.cols; j++){
+    //         if(tmp.at<uchar>(i, j) > 0)
+    //             continue;
+    //         else
+    //             tmp.at<uchar>(i, j) = 0;
+    //     }
+    // }
+    // return tmp;
+
+    // simplified
+    cv::Mat tmp;
+    cv::max(X, cv::Scalar(0), tmp);
+    return tmp;
+}
+
+cv::Mat utils :: dot (const cv::Mat & mat1, const cv::Mat & mat2) {
     assert(mat1.cols == mat2.rows);
 
     cv::Mat tmp(mat1.rows, mat2.cols, mat1.type());
 
     for (int i = 0; i < mat1.rows; i++) {
         for (int j = 0; j < mat2.cols; j++) {
-            tmp.at<uchar>(i, j) = 0; // Initialize to zero
+            tmp.at<uchar>(i, j) = 0;
 
             for (int z = 0; z < mat1.cols; z++) {
                 tmp.at<uchar>(i, j) += mat1.at<uchar>(i, z) * mat2.at<uchar>(z, j);
             }
         }
     }
-
     return tmp;
 }
 
@@ -48,12 +65,12 @@ mazeNet :: mazeNet (int in, int hide, int out) {
     output_size = out;
 
     w1 = cv::Mat (input_size, hidden_size, CV_32FC1); // Use CV_32FC1 for floating-point weights
-    randu(w1, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255)); // initiate random weigths
+    randu(w1, cv::Scalar(0.0), cv::Scalar(1.0)); // initiate random weigths
 
     b1 = cv::Mat::zeros(1, hidden_size, CV_32FC1);
 
     w2 = cv::Mat (hidden_size, output_size, CV_32FC1);
-    randu(w2, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
+    randu(w2, cv::Scalar(0.0), cv::Scalar(1.0));
 
     b2 = cv::Mat::zeros(2, output_size, CV_32FC1);
 
@@ -61,6 +78,8 @@ mazeNet :: mazeNet (int in, int hide, int out) {
 
 cv::Mat mazeNet :: forward (const cv::Mat & X){
     z1 = utils().dot(w1, w1) + b1;
+    a1 = utils().relu(z1);
+    return result;
 }
 
 void mazeNet :: printLayerSize () {
