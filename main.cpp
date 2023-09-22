b474a27f850bfc1687819e6bb92759aa289afb53
mahdizynali <zeinali.mahdy@.com>
2023-09-22 13:22:15 +0330

    class function declaration

--- main.cpp
+++ main.cpp
@@ -16,6 +16,33 @@ class mazeNet{
         void printLayerSize();
 };
 
+mazeNet :: mazeNet (int in, int hide, int out) {
+    input_size = in;
+    hidden_size = hide;
+    output_size = out;
+
+    w1 = cv::Mat (input_size, hidden_size, CV_8UC3);
+    randu(w1, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255)); // initiate random weigths
+
+    b1 = Mat::zeros(1, hidden_size, CV_8UC1);
+
+    w2 = cv::Mat (hidden_size, output_size, CV_8UC3);
+    randu(w2, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
+
+    b2 = Mat::zeros(2, output_size, CV_8UC1);
+
+}
+
+cv::Mat mazeNet :: forward (const cv::Mat & X){
+
+}
+
+void mazeNet :: printLayerSize () {
+    cout << "input size : " << input_size << endl;
+    cout << "hiden size : " << hidden_size << endl;
+    cout << "output size : " << output_size << endl;
+}
+
 int main(){
 
     mazeNet maze(784, 128, 10);
