6701f97fc4782cf088c24419c95c8b3cecbc602c
Mahdi Zeinali <zeinali.mahdy@gmail.com>
2026-05-14 09:56:29 +0330

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,17 +2,19 @@
 #define UTILS_HPP
 
 #include "config.hpp"
-
-class helper{
-
+class helper {
     public:
-        cv::Mat sigmoid (const cv::Mat &);
-        cv::Mat softmax (const cv::Mat &);
-        cv::Mat relu (const cv::Mat &);
-        cv::Mat dot (const cv::Mat &, const cv::Mat &);
-        cv::Mat sum (const cv::Mat &, const cv::Mat &);
-        cv::Mat sub (const cv::Mat &, const cv::Mat &);
-        double categoricalCrossEntropy (const cv::Mat &, const cv::Mat &);
+        cv::Mat sigmoid(const cv::Mat&);
+        cv::Mat softmax(const cv::Mat&);
+        cv::Mat relu(const cv::Mat&);
+        cv::Mat reluDerivative(const cv::Mat&);
+        cv::Mat oneHot(const cv::Mat&, int);
+    
+        cv::Mat dot(const cv::Mat&, const cv::Mat&);
+        cv::Mat sum(const cv::Mat&, const cv::Mat&);
+        cv::Mat sub(const cv::Mat&, const cv::Mat&);
+    
+        double categoricalCrossEntropy(const cv::Mat&, const cv::Mat&);
 };
 
 #endif // UTILS_HPP
